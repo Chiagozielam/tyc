dd2838289514744d1627c12682be2f71d8d234d6
Daniel Don <dandons25@gmail.com>
2019-04-06 21:57:46 +0100

    Updates

---
+++ precache-manifest.4dc4d95f8c374c1fb3bf24334bc140a8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "96ff6d3d3ab26a049ccbbb8ce9ee501a",
+    "url": "/Chiagozielam/tyc.git/static/media/forgiveness.96ff6d3d.jpg"
+  },
+  {
+    "revision": "82265868aba0a2e679cf",
+    "url": "/Chiagozielam/tyc.git/static/css/main.29ce1270.chunk.css"
+  },
+  {
+    "revision": "190ca9d2d2035d5d59cc",
+    "url": "/Chiagozielam/tyc.git/static/js/runtime~main.aa748829.js"
+  },
+  {
+    "revision": "4b823558fed9efb10ddc",
+    "url": "/Chiagozielam/tyc.git/static/js/2.8160efd7.chunk.js"
+  },
+  {
+    "revision": "248a8b2d7a96acf10c099a6c1109dfe5",
+    "url": "/Chiagozielam/tyc.git/static/media/joshwh2.248a8b2d.jpg"
+  },
+  {
+    "revision": "82265868aba0a2e679cf",
+    "url": "/Chiagozielam/tyc.git/static/js/main.a33ee63e.chunk.js"
+  },
+  {
+    "revision": "d85fb6ef092173a4c72761e4140ad04b",
+    "url": "/Chiagozielam/tyc.git/static/media/joshwh1.d85fb6ef.jpg"
+  },
+  {
+    "revision": "6ddbd3edd4fc44620e3d4b3a66838b4a",
+    "url": "/Chiagozielam/tyc.git/static/media/why.6ddbd3ed.jpg"
+  },
+  {
+    "revision": "6e86ca368aef2b62c04a6158f8e01b6e",
+    "url": "/Chiagozielam/tyc.git/static/media/TEOC.6e86ca36.pdf"
+  },
+  {
+    "revision": "e0ea49f86f3a00815a4fa7d1cfb94c13",
+    "url": "/Chiagozielam/tyc.git/static/media/WH_2017.e0ea49f8.pdf"
+  },
+  {
+    "revision": "38e85e8239171f72c0f614ebe9e54a4b",
+    "url": "/Chiagozielam/tyc.git/index.html"
+  }
+];
